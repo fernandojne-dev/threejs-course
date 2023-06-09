27ef5f85be26e66f1f26efcce747439fc1d159af
Fernando Nuñez <fernandojne.20@gmail.com>
2023-06-08 22:16:39 -0500

    Lesson 5: Introduce use of position, scale, rotation and lookAt methods

--- Chapter-01/05-transform-objects/src/script.js
+++ Chapter-01/05-transform-objects/src/script.js
@@ -14,6 +14,28 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// Positioning objects in 3D space
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+mesh.position.set(0.7, -0.6, 1)
+
+// Scale objects
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation objects
+// Gimbal lock is when you rotate an object in 3D space and it loses one of its axis
+// to solve this problem we can use reordering of the rotation axis, puting the axis Y in the first place
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
 /**
  * Sizes
  */
@@ -28,6 +50,9 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+// lookAt is a method that makes the camera look at a specific position
+camera.lookAt(mesh.position)
 
 /**
  * Renderer
