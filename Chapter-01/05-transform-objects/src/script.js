c96a400bb751650c6ba58aefa9b9687c8f67a7f9
Fernando Nuñez <fernandojne.20@gmail.com>
2023-06-08 22:21:45 -0500

    Lesson 5: Introduce groups of objects

--- Chapter-01/05-transform-objects/src/script.js
+++ Chapter-01/05-transform-objects/src/script.js
@@ -7,31 +7,33 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Objects
+ * Group
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
 
-// Positioning objects in 3D space
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
-mesh.position.set(0.7, -0.6, 1)
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
 
-// Scale objects
-// mesh.scale.x = 2
-// mesh.scale.y = 0.5
-// mesh.scale.z = 0.5
-mesh.scale.set(2, 0.5, 0.5)
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = -2
+group.add(cube2)
 
-// Rotation objects
-// Gimbal lock is when you rotate an object in 3D space and it loses one of its axis
-// to solve this problem we can use reordering of the rotation axis, puting the axis Y in the first place
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 2
+group.add(cube3)
 
 const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper)
@@ -50,9 +52,6 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
-
-// lookAt is a method that makes the camera look at a specific position
-camera.lookAt(mesh.position)
 
 /**
  * Renderer
